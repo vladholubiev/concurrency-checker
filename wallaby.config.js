4a3c633558461e182ea9e59f9b07bba748318218
Vlad Holubiev <golubev.vld@gmail.com>
2020-01-23 12:40:21 +0200

    feat: simplify wallaby.config.js

--- wallaby.config.js
+++ wallaby.config.js
@@ -1,30 +1,12 @@
-module.exports = wallaby => {
-  process.env.NODE_ENV = 'test';
-
+module.exports = () => {
   return {
-    testFramework: 'jest',
-    files: ['package.json', 'src/**/*.ts', '!src/**/*.test.ts', './common-mocks.js'],
+    autoDetect: true,
+    files: ['package.json', 'src/**/*.ts', '!src/**/*.test.ts'],
     tests: ['src/**/*.test.ts'],
     env: {
-      type: 'node',
-      runner: 'node'
-    },
-    compilers: {
-      'src/**/*.js': wallaby.compilers.babel(),
-      '**/*.ts?(x)': wallaby.compilers.typeScript()
-    },
-    setup(wallaby) {
-      wallaby.testFramework.configure(require('./package.json').jest);
-
-      process.env.TZ = 'UTC';
-    },
-    preprocessors: {
-      '**/*.js': file =>
-        require('@babel/core').transform(file.content, {
-          sourceMap: true,
-          compact: false,
-          filename: file.path
-        })
+      params: {
+        env: 'TZ=UTC'
+      }
     }
   };
 };
