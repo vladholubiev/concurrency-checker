caa8ab9eb64f3cf4838d07c94a614617620ce4c0
Vlad Holubiev <golubev.vld@gmail.com>
2020-03-26 19:04:48 +0200

    refactor: simplify wallaby config for api tests

--- wallaby-api-tests.config.js
+++ wallaby-api-tests.config.js
@@ -1,32 +1,12 @@
-module.exports = (wallaby) => {
-  process.env.NODE_ENV = 'test';
-
+module.exports = () => {
   return {
-    testFramework: 'jest',
-    files: ['package.json', './common-mocks.js', 'api-tests/**/*.ts', '!api-tests/**/*.test.ts'],
+    autoDetect: true,
+    files: ['package.json', 'api-tests/**/*.ts', '!api-tests/**/*.test.ts'],
     tests: ['api-tests/**/*.test.ts'],
     env: {
-      type: 'node',
-      runner: 'node'
-    },
-    compilers: {
-      'api-tests/**/*.js': wallaby.compilers.babel(),
-      '**/*.ts?(x)': wallaby.compilers.typeScript()
-    },
-    setup(wallaby) {
-      wallaby.testFramework.configure(require('./package.json').jest);
-
-      process.env.TZ = 'UTC';
-      process.env.ENVIRONMENT = 'staging';
-      process.env.API_HOST = 'api.gsstaging.net';
-    },
-    preprocessors: {
-      '**/*.js': (file) =>
-        require('@babel/core').transform(file.content, {
-          sourceMap: true,
-          compact: false,
-          filename: file.path
-        })
+      params: {
+        env: 'TZ=UTC;ENVIRONMENT=staging;API_HOST=api.gsstaging.net'
+      }
     }
   };
 };
