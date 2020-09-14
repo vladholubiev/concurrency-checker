5b5cfad64734179bffb742193f119e6bc0df894b
Vlad Holubiev <golubev.vld@gmail.com>
2020-09-14 20:31:31 +0300

    fix: add './common-mocks.js' to wallaby js

--- wallaby.config.js
+++ wallaby.config.js
@@ -1,7 +1,7 @@
 module.exports = () => {
   return {
     autoDetect: true,
-    files: ['package.json', 'src/**/*.ts', '!src/**/*.test.ts'],
+    files: ['package.json', 'src/**/*.ts', '!src/**/*.test.ts', './common-mocks.js'],
     tests: ['src/**/*.test.ts'],
     env: {
       params: {
