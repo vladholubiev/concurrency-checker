336c9640040e364c5f516d45751092492e197aa2
Vlad Holubiev <golubev.vld@gmail.com>
2022-02-28 21:25:59 +0100

    feat: add missing provider

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,8 +1,10 @@
 resource "aws_iam_role" "main" {
+  provider           = aws.us_east_1
   assume_role_policy = data.aws_iam_policy_document.main.json
 }
 
 resource "aws_iam_role_policy_attachment" "main" {
+  provider   = aws.us_east_1
   role       = aws_iam_role.main.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
--- infra/lambda.tf
+++ infra/lambda.tf
@@ -12,6 +12,7 @@ resource "aws_lambda_function" "process" {
 }
 
 resource "aws_lambda_event_source_mapping" "process" {
+  provider         = aws.us_east_1
   event_source_arn = aws_sqs_queue.requests.arn
   function_name    = aws_lambda_function.process.arn
 }
