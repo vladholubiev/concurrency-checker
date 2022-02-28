07161bcdc425764047ebc509c3dab8b1d0f0ba53
Vlad Holubiev <golubev.vld@gmail.com>
2022-02-28 22:31:44 +0100

    feat: accept headers

--- sqs-dispatch.ts
+++ sqs-dispatch.ts
@@ -30,6 +30,24 @@ const REGIONS = [
 const payload = {
   url: `https://my.crb-dnr.ru/rich/auth`,
   method: 'GET',
+  headers: {
+    accept:
+      'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'en-US,en;q=0.9,uk;q=0.8,ru;q=0.7',
+    'cache-control': 'max-age=0',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    cookie:
+      'localize=ru; gatetype=IC; localize=ru; CLIENTUUID=47D1434A89314F4D9F1F4A3931E8041E; JSESSIONID=40D74AEA646C772F4AD7EE0707EB108C',
+    Referer: 'https://my.crb-dnr.ru/rich/auth',
+    'Referrer-Policy': 'strict-origin-when-cross-origin',
+  },
 };
 
 (async () => {
--- src/process/handler.ts
+++ src/process/handler.ts
@@ -6,7 +6,7 @@ import {times} from 'lodash';
 export const handler: SQSHandler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  const info: {url: string; method: string; body?: string} = JSON.parse(event.Records[0].body);
+  const info: any = JSON.parse(event.Records[0].body);
   const data = times(10000, () => info);
 
   await pMap(data, makeRequest, {concurrency: 500, stopOnError: false});
@@ -20,6 +20,10 @@ async function makeRequest(item: any): Promise<void> {
 
   if (item.body) {
     req.body = item.body;
+  }
+
+  if (item.headers) {
+    req.headers = item.headers;
   }
 
   const resp = await fetch(item.url, req);
