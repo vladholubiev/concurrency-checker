d2c7e334046f4dec424b9d7dc810f7dc94e6efbf
Vlad Holubiev <golubev.vld@gmail.com>
2022-02-28 22:12:59 +0100

    feat: set all regions

--- sqs-dispatch.ts
+++ sqs-dispatch.ts
@@ -2,7 +2,30 @@ import AWS from 'aws-sdk';
 import pMap from 'p-map';
 import {times} from 'lodash';
 
-const REGIONS = ['us-east-1'];
+const REGIONS = [
+  'us-east-1',
+  'us-east-2',
+  'us-west-1',
+  'us-west-2',
+  'af-south-1',
+  'ap-east-1',
+  'ap-southeast-3',
+  'ap-southeast-1',
+  'ap-southeast-2',
+  'ap-south-1',
+  'ap-northeast-3',
+  'ap-northeast-2',
+  'ap-northeast-1',
+  'ca-central-1',
+  'eu-central-1',
+  'eu-west-1',
+  'eu-west-2',
+  'eu-west-3',
+  'eu-south-1',
+  'eu-north-1',
+  'me-south-1',
+  'sa-east-1',
+];
 
 const payload = {
   url: `https://my.crb-dnr.ru/rich/auth`,
