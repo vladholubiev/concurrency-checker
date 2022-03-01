f210b621bd2524319868016fe6d07199d7b6d3b3
Vlad Holubiev <golubev.vld@gmail.com>
2022-03-01 11:42:21 +0100

    fix: make sqs visibility to 60s

--- infra/modules/concurrency-checker/sqs.tf
+++ infra/modules/concurrency-checker/sqs.tf
@@ -2,5 +2,5 @@ resource "aws_sqs_queue" "requests" {
   provider                   = aws.primary
   name                       = "requests"
   max_message_size           = 256 * 1024
-  visibility_timeout_seconds = 360
+  visibility_timeout_seconds = 60
 }
